81a487c631047c10b40747d34e7158b68d4489ca
Zach Liss <zach.liss@dockyard.com>
2019-09-13 11:51:11 -0400

    live counter!

--- lib/zorcle_web/live/mascot_game_live.ex
+++ lib/zorcle_web/live/mascot_game_live.ex
@@ -3,13 +3,21 @@ defmodule ZorcleWeb.MascotGameLive do
 
   # called when the liveview is connected to
   def mount(_params, socket) do
-    {:ok, socket}
+    send(self(), :count)
+    {:ok, assign(socket, :count, 0)}
   end
 
   # called whenever there is a state change and delivers new html down to the client to be diffed and "DOM updatedededed"
   def render(assigns) do
     ~L"""
     Welcome to the Mascot Game
+    Count: <%= @count %>
     """
+  end
+
+  def handle_info(:count, socket) do
+    Process.send_after(self(), :count, 1000)
+    count = socket.assigns.count + 1
+    {:noreply, assign(socket, :count, count)}
   end
 end
