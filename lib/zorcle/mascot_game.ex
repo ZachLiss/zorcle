9b7fd7241c6a3c57c4e0245eba3a2458984f8381
Zach Liss <zach.liss@dockyard.com>
2019-12-22 07:51:04 -0500

    move implementation details to game.ex

--- lib/zorcle/mascot_game.ex
+++ lib/zorcle/mascot_game.ex
@@ -1,7 +1,7 @@
 defmodule Zorcle.MascotGame do
   use GenServer
 
-  alias Zorcle.MascotGame.Questions
+  alias Zorcle.MascotGame.{Game, Questions}
 
   def start_link(_) do
     IO.puts("STARTING THE GAME GenServer")
@@ -13,33 +13,21 @@ defmodule Zorcle.MascotGame do
   end
 
   defp initial_state do
-    %{
-      users: %{},
-      game_status: :not_started,
-      # we'll think about how to represent the used questions state
-      used_questions: %{},
-      current_question: %{school: nil},
-      winning_user: ""
-    }
+    Game.new()
   end
 
   def handle_call({:user_join, user_name}, {_pid, _ref}, %{users: users} = state) do
     IO.puts("#{user_name} is joining the game")
-    users = Map.put(users, user_name, 0)
-    state = Map.put(state, :users, users)
 
-    game_state = %{
-      # how to handle cases when properties may not exist?
-      # before the game is started the current_question wont have a school
-      current_question_school: state.current_question.school,
-      users: state.users,
-      game_status: state.game_status
-    }
+    case Game.add_user(state, user_name) do
+      {:ok, game} ->
+        broadcast_updated_game_state(game)
+        {:reply, :ok, game}
 
-    broadcast_updated_game_state(state)
-
-    # we may want to return a value, handle situations where users cannot join
-    {:reply, game_state, state}
+      {:error, _} ->
+        # silently fail for now
+        {:reply, :ok, state}
+    end
   end
 
   # how to handle "pushing" the updated state back to the LV? is that a thing we should do?
@@ -47,68 +35,29 @@ defmodule Zorcle.MascotGame do
   def handle_call({:start_game}, _pid, state) do
     IO.puts("Starting game")
 
-    state =
-      state
-      |> Map.put(:game_status, :started)
-      |> Map.put(:current_question, get_random_question())
-      |> broadcast_updated_game_state
-
-    {:reply, :ok, state}
+    {:ok, game} = Game.start_game(state)
+    broadcast_updated_game_state(game)
+    {:reply, :ok, game}
   end
 
   def handle_call({:end_game}, _pid, state) do
-    users_with_no_score = Map.new(state.users, fn {k, _v} -> {k, 0} end)
+    IO.puts("Ending game")
 
-    state =
-      initial_state()
-      |> Map.put(:users, users_with_no_score)
-      |> broadcast_updated_game_state
-
-    {:reply, :ok, state}
-  end
-
-  def handle_call({:get_game_state}, _pid, state) do
-    game_state = %{
-      current_question_school: state.current_question.school,
-      users: state.users,
-      game_status: state.game_status
-    }
-
-    {:reply, game_state, state}
+    {:ok, game} = Game.end_game(state)
+    broadcast_updated_game_state(game)
+    {:reply, :ok, game}
   end
 
   def handle_call({:answer_question, guess, user_name}, _pid, state) do
     IO.puts("Correct Answer: #{state.current_question.mascot}")
 
-    case(state.current_question.mascot == guess) do
-      true ->
-        state =
-          state
-          |> increase_score_for_user(user_name)
-          |> check_for_winning_user(user_name)
-          |> Map.put(:current_question, get_random_question())
-          |> broadcast_updated_game_state
-
-        {:reply, :ok, state}
+    case(Game.answer_question(state, guess, user_name)) do
+      {:ok, game} ->
+        broadcast_updated_game_state(game)
+        {:reply, :ok, game}
 
       _ ->
         {:reply, :incorrect, state}
-    end
-  end
-
-  defp increase_score_for_user(state, user_name) do
-    users =
-      state.users
-      |> Map.put(user_name, state.users[user_name] + 1)
-
-    Map.put(state, :users, users)
-  end
-
-  defp check_for_winning_user(%{users: users} = state, user_name) do
-    if Map.get(users, user_name) >= 3 do
-      Map.put(state, :winning_user, user_name)
-    else
-      state
     end
   end
 
@@ -122,6 +71,7 @@ defmodule Zorcle.MascotGame do
     state
   end
 
+  # move this to the mascot game live module?
   defp state_for_lv(state) do
     # return some subset of the state from the GenServer to be consumed by mascot_game_live
     %{
@@ -142,26 +92,6 @@ defmodule Zorcle.MascotGame do
   end
 
   def end_game() do
-    IO.puts("ending game")
     GenServer.call(__MODULE__, {:end_game})
-  end
-
-  # might not need this anymore since we can push state with our PubSub.broadcast
-  def get_game_state do
-    GenServer.call(__MODULE__, {:get_game_state})
-  end
-
-  def list_questions do
-    Questions.get_questions()
-  end
-
-  def get_random_question do
-    Questions.get_questions()
-    |> Enum.random()
-  end
-
-  def check_answer(guess, user) do
-    IO.puts("User: #{user.name}")
-    GenServer.call(__MODULE__, {:answer_question, guess, user.name})
   end
 end
--- lib/zorcle/mascot_game/game.ex
+++ lib/zorcle/mascot_game/game.ex
@@ -14,18 +14,83 @@ defmodule Zorcle.MascotGame.Game do
   end
 
   def add_user(%Game{users: users} = game, user_name) do
-    updated_users = Map.put(users, user_name, 0)
-    %Game{game | users: updated_users}
-  end
+    case game.game_status do
+      :not_started ->
+        updated_users = Map.put(users, user_name, 0)
+        {:ok, %Game{game | users: updated_users}}
 
-  def get_user_score(%Game{users: users}, user_name) do
-    case Map.fetch(users, user_name) do
-      :error -> 0
-      {:ok, score} -> score
+      :started ->
+        {:error, :already_started}
+
+      _ ->
+        {:error, :game_ended}
     end
   end
 
-  # start game
-  # end game
+  # def get_user_score(%Game{users: users}, user_name) do
+  #   case Map.fetch(users, user_name) do
+  #     {:ok, score} -> score
+  #     :error -> 0
+  #   end
+  # end
+
+  def start_game(%Game{} = game) do
+    game =
+      game
+      |> Map.put(:game_status, :started)
+      |> Map.put(:current_question, get_random_question())
+
+    {:ok, game}
+  end
+
+  def end_game(%Game{} = game) do
+    game =
+      game
+      |> Map.put(:game_status, :ended)
+
+    {:ok, game}
+  end
+
   # answer question
+  def answer_question(%Game{current_question: current_question} = game, guess, user_name) do
+    case(current_question.mascot == guess) do
+      true ->
+        game =
+          game
+          |> increase_score_for_user(user_name)
+          |> check_for_winning_user(user_name)
+          |> Map.put(:current_question, get_random_question())
+
+        {:ok, game}
+
+      _ ->
+        {:error, :incorrect}
+    end
+  end
+
+  defp increase_score_for_user(%Game{} = game, user_name) do
+    users =
+      game.users
+      |> Map.put(user_name, game.users[user_name] + 1)
+
+    Map.put(game, :users, users)
+  end
+
+  defp check_for_winning_user(%Game{users: users} = game, user_name) do
+    if Map.get(users, user_name) >= @winning_score do
+      Map.put(game, :winning_user, user_name)
+    else
+      game
+    end
+  end
+
+  ### pulled from Zorcle.MascotGame
+  def list_questions do
+    Questions.get_questions()
+  end
+
+  def get_random_question do
+    Questions.get_questions()
+    |> Enum.random()
+  end
 end
